4ec3fd0b60e400c75bbffdaa28426469030c56a2
Aleš Fabiánek <fabianek.ales@gmail.com>
2023-01-02 12:07:20 +0100

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+export * from "./generators/AvailabilityModelGenerator";
+
+export * from "./generators/AvailabilityCalendarModelGenerator";
+
+export * from "./generators/BookingModelGenerator";
+
+export * from "./generators/OptionModelGenerator";
+
+export * from "./generators/ProductModelGenerator";
+
 export * from "./generators/UnitModelGenerator";
+
+export * from "./parsers/AvailabilityCalendarParser";
+
+export * from "./parsers/AvailabilityParser";
+
+export * from "./parsers/BookingParser";
+
+export * from "./parsers/OptionParser";
+
+export * from "./parsers/ProductParser";
 
 export * from "./parsers/UnitParser";
