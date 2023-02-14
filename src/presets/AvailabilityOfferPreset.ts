57ffcd7a371a68c45e94cb06bce96da43f72ba0f
Aleš Fabiánek <fabianek.ales@gmail.com>
2023-02-14 14:12:08 +0100

    Renamed class names #2

--- src/presets/AvailabilityOfferPreset.ts
+++ src/presets/AvailabilityOfferPreset.ts
@@ -1,12 +1,12 @@
 import { AvailabilityOffers } from "@octocloud/types";
 import { AvailabilityOffersModel } from "../models/availability/AvailabilityOffersModel";
-import { OfferPresets } from "./OfferPreset";
+import { OfferPreset } from "./OfferPreset";
 import { AvailabilityParser } from "../parsers/AvailabilityParser";
 
 export abstract class AvailabilityOfferPreset {
   private static readonly availabilityOfferParser: AvailabilityParser = new AvailabilityParser();
 
-  private static readonly offerModel = OfferPresets.TEN_PERCENT_OFF_MODEL;
+  private static readonly offerModel = OfferPreset.TEN_PERCENT_OFF_MODEL;
 
   public static TEN_PERCENT_OFF_MODEL: AvailabilityOffersModel = new AvailabilityOffersModel({
     offerCode: this.offerModel.code,
